4173488070b11b96c80dde8acad8b220c5c3e0c2
YH <devyhan93@gmail.com>
2020-02-24 16:13:38 +0900

    DatePicker 03

--- DataPicker/DataPicker/ViewController.swift
+++ DataPicker/DataPicker/ViewController.swift
@@ -14,26 +14,31 @@ class ViewController: UIViewController {
         // timeInterval: 타이머 간격 | target: 동작될 view | selector: 타이머가 구동될 때 실행할 함수 | userInfo: 사용자 정보 | repeats: 반복 여부
         lblPickerTime.text = "선택한 시간 없음" // 타임 피커의 액션이 없을때 lblPickTime에 "선택한 시간 없음" 표시
     }
-
+    
     @IBAction func changeDatePicker(_ sender: UIDatePicker) { // 데이트 피커에 대한 액션 함수
         let datePickerView = sender // 데이트 피커를 선택할 때 발생하는 액션 함수인 "changeDatePicker"가 호출되면서 sender 자료형 인수 전달받음
         
         let formatter = DateFormatter() // 날짜를 출력하기 위해 DataFormatter라는 클래스 상수 formatter를 선언
         formatter.dateFormat = "yyyy-MM-dd HH:mm EEE" // dataFormat설정 "년-월-알 시:분:초 요일"
         
-            lblPickerTime.text = formatter.string(from: datePickerView.date)
-            // 데이트 피커에서 선택한 날짜를 설정한 포멧대로 String메서드를 사용하여 문자열로 변환 lblPickerTime = "선택시간: " + "문자열로 변환한 선택한 날짜"
+        lblPickerTime.text = formatter.string(from: datePickerView.date)
+        // 데이트 피커에서 선택한 날짜를 설정한 포멧대로 String메서드를 사용하여 문자열로 변환 lblPickerTime = "선택시간: " + "문자열로 변환한 선택한 날짜"
     }
     
     @objc func updateTime() { // 타이머 동작 함수
         // Swift4 에서는 #selector()의 인자로 사용될 메서드 를 선언할때 Objective-C와의 호환성을 위해 함수 앞에 반드시 @objc키워드를 붙여야함
-        let date = NSDate() // NSDAte 함수를 사용하여 가져옴
+        let date = NSDate() // NSDAte 함수를 사용하여 현재시간을 가져옴
         
         let formatter = DateFormatter() // 날짜를 출력하기 위해서 DateFormatter라는 클래스 상수 formatter를 선언
-        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss EEE" // formatter의 dateFormat 속성 설정
+        formatter.dateFormat = "yyyy-MM-dd HH:mm EEE" // formatter의 dateFormat 속성 설정
         lblCurrentTime.text = formatter.string(from: date as Date)
         // formatter.string(date)는 피커 뷰에서 선택한 날짜를 formatter의 dateFormat에서 설정한 포맷대로 string메서드를 사용하여 문자열로 변환
+        
+        if(lblPickerTime.text == lblCurrentTime.text) { // lblPickerTime.text와 lblCurrentTime.text가 같은지 확인하는 분기
+            view.backgroundColor = UIColor.red // 조건이 true일 view의 배경화면을 빨간색으로 변경
+        } else {
+            view.backgroundColor = UIColor.white
+        }
     }
-    
 }
 
