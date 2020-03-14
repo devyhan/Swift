0a6acef9175120d44c002b8d53dc2a4421ec0fe3
devyhan93 <devyhan93@gmail.com>
2020-03-14 21:00:29 +0900

    Alert init

---
+++ Alert/Alert/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Alert
+//
+//  Created by 요한 on 2020/03/14.
+//  Copyright © 2020 요한. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
